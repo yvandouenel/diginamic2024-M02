64839ab5c2f79e537bbe81dde74bc88bdb2f72b2
yvandouenel <y.douenel@coopernet.fr>
2024-04-04 12:47:27 +0200

    fonctions callback.

---
+++ javascript/03function.js
@@ -0,0 +1,95 @@
+// fonction anonyme immédiate (IIFES) qui permet d'éviter la création de variables globales
+const digiObject = (function () {
+  /**
+   * Fonction de test
+   * @returns String
+   */
+  function test() {
+    return "fonction un peu dégeu";
+  }
+
+  let t = test(); // passage par référence car test est de type évolué (function)
+  console.log(`t`, t);
+  console.log(`t`, typeof t);
+  /**
+   * Renvoie l'argument multiplié par 5
+   * @param {number} num 
+   * @returns {number}
+   */
+  /* function multiplyByFive(num) {
+    return num * 5;
+  } */
+  multiplyByFive = num => num * 5;
+  // multiplyByFive(5);
+  console.log(multiplyByFive(5));// 5 est un argument
+
+  let i = 1;
+  function a() {
+    let j = 2;
+    console.log(`j`, j);
+    b();
+    console.log(`b`, b);
+    function b() {
+      let l = 4;
+      console.log(l);
+      {
+        let k = 3;
+        console.log(k);
+        console.log(l);
+        console.log(i);
+      }
+
+    }
+  }
+  a();
+  console.log(`i`, i);
+  console.log(`a`, a);
+
+  // closure
+  function makeBankAccount() {
+    // this one is private
+    let balance = 0;
+
+    // a "getter", made public via the returned object below
+    function getBalance() {
+      return balance;
+    }
+
+    // a "setter", made public via the returned object below
+    function setBalance(amount) {
+      // ... validate before updating the balance
+      balance = amount;
+    }
+
+    return {
+      // ...
+      getBalance,
+      setBalance
+    };
+  }
+
+  const account = makeBankAccount();
+  account.setBalance(100);
+  // comment afficher la variable balance ?
+  console.log("balance = ", account.getBalance());
+  return {
+    multiplyByFive
+  }
+})();
+
+const result = digiObject.multiplyByFive(10);
+console.log(`result`, result);
+
+function success(result) {
+  console.log(`Bravo ! ton score est : ${result}`);
+}
+function fail(result) {
+  console.log(`Désolé, vous avez perdu ! ton petit score: ${result}`);
+}
+function game(s, f) {// on dit que s et f sont des callbacks
+  const result = Math.random();
+  if (result > 0.5) {
+    s(result);
+  } else f(result)
+}
+game(success, fail);// ici les arguments sont de type function
