462fa4fc2e8773029d3dc3879fc213d6ea9000c9
yvandouenel <y.douenel@coopernet.fr>
2024-04-08 14:36:24 +0200

    Explication des promesses.

---
+++ javascript/08TestPromise.js
@@ -0,0 +1,37 @@
+function loadToken() {
+  return new Promise((resolve, reject) => {
+    setTimeout(function () {
+      if (Math.random() > 0.5) {
+        resolve("SDddsiopdfqsdfoeeld;tedDD");// Envoi au then "Glop" qui sera récupéré dans le paramètre
+      } else {
+        reject("Erreur dans loadToken");
+      }
+    }, 2000)
+  })
+}
+function loadUser(token) {
+  return new Promise((resolve, reject) => {
+    setTimeout(function () {
+      if (Math.random() > 0.5) {
+        resolve({ name: "toto", id: 1 });// Envoi au then l'objet qui représente l'utilisateur
+      } else {
+        reject("Erreur dans loadUser");
+      }
+    }, 2000)
+  })
+}
+loadToken()
+  // si la promesse est tenue, on va dans le then
+  .then(token => {
+    console.log(`msg`, token);
+    return loadUser(token); // On chaine les promesses 
+  })
+  .then(user => {
+    console.log(`user`, user);
+    // Cas favorable deux fois : lors de l'appel de loadToken puis de loadUser
+  })
+  // Si la promesse n'est pas tenue, on passe dans le catch
+  .catch(error => {
+    console.error(error);
+  })
+console.log(`après l'appel de loadToken`);
