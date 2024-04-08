56b17bb6cbdb2c159b1ddace0acdf132329c9def
yvandouenel <y.douenel@coopernet.fr>
2024-04-08 12:35:47 +0200

    Gestion du submit du formulaire.

--- javascript/components/TodoList.js
+++ javascript/components/TodoList.js
@@ -5,7 +5,17 @@ export default class TodoList extends Dom {
     // Référence à l'élément du DOM existant qui a pour id "root"
     this.rootDom = document.getElementById("root");
 
-    this.render();
+    this.domElts = this.render();
+
+    // Gestion des événements
+    this.manageEvents();
+  }
+  manageEvents() {
+    this.domElts.form.addEventListener("submit", (event) => {
+      // Supprimer l'appel de la requête http via l'action du formulaire avec la méthode GET
+      event.preventDefault();
+      console.log(`Dans submit addEventListener`);
+    })
   }
   render() {
     // Création du formulaire
@@ -15,5 +25,9 @@ export default class TodoList extends Dom {
     const buttonSubmit = this.createMarkup("button", "Ajouter la tâche", form, { "id": "task", type: "submit" });
     // Création de l'élément section qui comprend toutes les tâches
     this.sectionListTasks = this.createMarkup("section", "", this.rootDom);
+
+    return {
+      form
+    }
   }
 }
