84d59a47472c2c4b5755a37209534f7afacbc2aa
yvandouenel <y.douenel@coopernet.fr>
2024-04-08 10:43:55 +0200

    Gestion du this avec une fonction fléchée.

--- javascript/components/Task.js
+++ javascript/components/Task.js
@@ -5,15 +5,35 @@ export default class Task {
     this.done = done;
     this.parent = parent;
 
-    // Affichage
-    this.render();
-  }
+    // Affichage et récupération des références vers les éléments du DOM constitutifs de la tâches
+    this.domElts = this.render();
 
+    // Gestion des événements
+    this.manageEvents();
+  }
+  manageEvents() {
+    this.domElts.buttonDeleteElt.addEventListener("click", () => {
+      console.log(`bouton delete cliqué`);
+      articleElt.remove();
+    })
+    this.domElts.buttonValidateElt.addEventListener("click", () => {
+      console.log(`bouton validate cliqué`);
+      this.done = !this.done;
+      console.log(this);
+      this.domElts.h2Elt.classList.toggle("done");
+    })
+  }
   render() {
     // création des éléments du DOM constitutifs de la tâche
     const articleElt = this.createMarkup("article", "", this.parent);
     const h2Elt = this.createMarkup("h2", this.name, articleElt);
-
+    const buttonDeleteElt = this.createMarkup("button", "Supprimer", articleElt);
+    const buttonValidateElt = this.createMarkup("button", "Validate", articleElt);
+    return ({
+      h2Elt,
+      buttonDeleteElt,
+      buttonValidateElt
+    })
   }
   /**
    * Crée un élément du dom, lui ajoute du texte, le place comme dernier
